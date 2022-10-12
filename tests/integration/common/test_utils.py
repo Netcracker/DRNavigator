72df973117f6918390791928b687ff9db4e43162
Imadzuma <mar-gosha98@mail.ru>
2022-10-12 15:54:27 +0300

    fix smclient_some

--- tests/integration/common/test_utils.py
+++ tests/integration/common/test_utils.py
@@ -7,13 +7,13 @@ import smclient
 
 def run_sm_client_command_with_exit(args, expected_exit_code=0):
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        smclient_some.main(args)
+        smclient.main(args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == expected_exit_code
 
 
 def run_sm_client_command(args):
-    smclient_some.main(args)
+    smclient.main(args)
 
 
 # TODO: temporary solution
