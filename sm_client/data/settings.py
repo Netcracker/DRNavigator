1ebc4092080d4ef6b58f080ff71b69aa13be57dc
Imadzuma <mar-gosha98@mail.ru>
2023-03-24 12:15:16 +0300

    add description comments result lists

--- sm_client/data/settings.py
+++ sm_client/data/settings.py
@@ -11,12 +11,14 @@ state_restrictions = {}
 FRONT_HTTP_AUTH = False
 SERVICE_DEFAULT_TIMEOUT = None
 
+# Result filter
+done_services = []              # Services, that were successfully done
+ignored_services = []           # Services, that were ignored because of --run-services or --skip-services option
+failed_services = []            # Services, that were failed and broke the procedure
+warned_services = []            # Services, that were failed, but didn't break the procedure (e.g. failed standby part for failover)
+skipped_due_deps_services = []  # Services, that were skipped, because they depend on failed ones or not finished because of previous flow step failed
+
 # Parameters from command line
-done_services = []
-ignored_services = []
-failed_services = []
-warned_services = []
-skipped_due_deps_services = []
 run_services = []
 skip_services = []
 force = False
