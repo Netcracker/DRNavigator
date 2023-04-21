5c26c110902b5ddb3c40b0af1624fe4f6a189cb4
Dmitry Yarovov <dmitrii.yarovov@netcracker.com>
2023-04-21 15:06:04 +0300

    refactoring tests

--- tests/cloud_test/tests/site-manager_test.py
+++ tests/cloud_test/tests/site-manager_test.py
@@ -55,92 +55,92 @@ class SitemanagerTestCase:
     def test_sm_incorrect_token(self, sm_env, config_dir):
         logging.info("TEST SITE-MANAGER WITH WRONG TOKEN")
         url = 'https://' + sm_env['host_name'] + "/sitemanager"
-        token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer test"
-            }
-        else:
-            headers = {}
+        headers = {
+            "Authorization": f"Bearer test"
+        }
+
         resp = requests.get(url, headers=headers, verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
         assert resp.status_code == 403
 
     def test_sm_empty_token(self, sm_env, config_dir):
         logging.info("TEST SITE-MANAGER WITH EMPTY TOKEN")
         url = 'https://' + sm_env['host_name'] + "/sitemanager"
-        token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer "
-            }
-        else:
-            headers = {}
-        resp = requests.get(url, headers=headers, verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
-        assert resp.status_code == 403
+
+        resp = requests.get(url, verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+        assert resp.status_code == 401
 
     def test_sm_check_incorrect_services(self, sm_env, config_dir):
         logging.info("TEST SITE-MANAGER WITH WRONG SERVICES")
         url = 'https://' + sm_env['host_name'] + "/sitemanager"
         token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
-        else:
-            headers = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
         http_body = {"procedure": "status", "run-service": "not-exist"}
         resp = requests.post(url, json=http_body, headers=headers,
                              verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+        resp_json = resp.json()
+        assert "Service doesn't exist" == resp_json['message']
         assert resp.status_code == 400
 
     def test_sm_check_incorrect_procedure(self, sm_env, config_dir):
         logging.info("TEST SITE-MANAGER WITH WRONG PROCEDURE")
         url = 'https://' + sm_env['host_name'] + "/sitemanager"
         token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
-        else:
-            headers = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
 
-        http_body = {"procedure": "lock", "run-service": "service-a"}
+        http_body = {"procedure": "lock", "run-service": "service-a.test-services"}
         resp = requests.post(url, json=http_body, headers=headers,
                              verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+        resp_json = resp.json()
+        assert "You should define procedure from list: ['active', 'standby', 'disable', 'list', 'status']" == resp_json[
+            'message']
         assert resp.status_code == 400
 
     def test_check_services(self, sm_env, config_dir, config_ingress_service):
         logging.info("TEST CHECK SERVICES STATUS")
 
+        url = 'https://' + sm_env['host_name'] + "/sitemanager"
         token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
-        else:
-            headers = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
         # Disable warnings about unsecure tls connection
         urllib3.disable_warnings(InsecureRequestWarning)
         # Check services connectivity
         logging.info(f"Check status services")
 
-        for ingress_services in config_ingress_service.values():
-            status_code = requests.get(f"http://{ingress_services}/sitemanager", headers=headers,
-                                       verify=config_dir['template_env']['sites']['site_1']['ca_cert']).status_code
-            assert status_code in [200, 204]
+        for service_name, ingress_services in config_ingress_service.items():
+            http_body = {"procedure": "status", "run-service": service_name}
+            resp_status = requests.get(f"http://{ingress_services}/sitemanager", headers=headers,
+                                       verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+            resp_healthz = requests.get(f"http://{ingress_services}/healthz", headers=headers,
+                                        verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+            resp_sm = requests.post(url, json=http_body, headers=headers,
+                             verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+
+            resp_status_json = resp_status.json()
+            resp_healthz_json = resp_healthz.json()
+            resp_sm_json = resp_sm.json()
+
+            service_status = {"healthz": resp_healthz_json.get("status", "--"),
+                              "mode": resp_status_json.get("mode", "--"),
+                              "status": resp_status_json.get("status", "--"),
+                              "message": resp_status_json.get("message", "")}
+            assert service_status == resp_sm_json['services'][service_name]
+            assert resp_status.status_code, resp_healthz.status_code in [200, 204]
 
     def test_check_services_in_sm(self, sm_env, config_dir, config_ingress_service):
         logging.info("TEST CHECK SERVICES ON SITE-MANAGER")
         url = 'https://' + sm_env['host_name'] + "/sitemanager"
         token = sm_env['token-sm']
-        if token is not None:
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
-        else:
-            headers = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
         resp = requests.get(url, headers=headers,
-                                verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
+                            verify=config_dir['template_env']['sites']['site_1']['ca_cert'])
 
         resp_status_code = resp.status_code
         assert resp_status_code in [200, 204]
