cb085037b679e68b888d0fcfd0714736a355c35e
Imadzuma <mar-gosha98@mail.ru>
2023-06-28 14:02:26 +0300

    fix test

--- tests/selftest/sm_client/test_processing.py
+++ tests/selftest/sm_client/test_processing.py
@@ -88,6 +88,7 @@ def test_io_http_json_request():
 
 
 def test_runservice_engine(caplog):
+    init_and_check_config(args_init())
     def process_node(node):
         node = ServiceDRStatus({'services': {node: {}}})
         if node.service in test_failed_services:
@@ -116,6 +117,7 @@ def test_runservice_engine(caplog):
 
 
 def test_runservice_engine_with_dependencies(caplog):
+    init_and_check_config(args_init())
     def process_node(node):
         node = ServiceDRStatus({'services': {node: {}}})
         if node.service in test_failed_services:
