3963140854cef3c310533c49ffb3a094d3bc72a4
Imadzuma <mar-gosha98@mail.ru>
2023-11-07 10:07:56 +0300

    fix token channel

--- site-manager-cr-controller/pkg/app/app.go
+++ site-manager-cr-controller/pkg/app/app.go
@@ -67,7 +67,7 @@ func Serve(bindAddress string, bindWebhookAddress string, certFile string, keyFi
 }
 
 // handleToken gets token of sm-auth-sa from kubernetes when it updates
-func handleToken(TokenChannel chan string, errChannel chan error) {
+func handleToken(tokenChannel chan string, errChannel chan error) {
 	logger := logger.SimpleLogger()
 	config, err := kube_config.GetKubeConfig()
 	if err != nil {
@@ -94,48 +94,52 @@ func handleToken(TokenChannel chan string, errChannel chan error) {
 		return
 	}
 
+	var token string
 	for {
-		event, ok := <-watcher.ResultChan()
-		if !ok {
-			logger.Errorf("Watch SA event channel is closed")
-			errChannel <- fmt.Errorf("can't handle token: channel is closed")
-			return
-		}
-		serviceAccount, ok := event.Object.(*corev1.ServiceAccount)
-		if !ok {
-			logger.Errorf("can't get SA from event: %s")
-			errChannel <- fmt.Errorf("can't handle SA from watching event")
-			return
-		}
-		if serviceAccount.GetName() != smServiceAccountName {
-			continue
-		}
-		if event.Type == watch.Added || event.Type == watch.Modified {
-			secretRef := utils.FindFirstFromSlice(serviceAccount.Secrets, func(secretRef corev1.ObjectReference) bool {
-				return strings.Contains(secretRef.Name, "token")
-			})
-			if secretRef == nil {
-				logger.Warnf("Secret for appropriate SA is not ready yet")
+		select {
+		case tokenChannel <- token:
+		case event, ok := <-watcher.ResultChan():
+			if !ok {
+				logger.Errorf("Watch SA event channel is closed")
+				errChannel <- fmt.Errorf("can't handle token: channel is closed")
+				return
+			}
+			serviceAccount, ok := event.Object.(*corev1.ServiceAccount)
+			if !ok {
+				logger.Errorf("can't get SA from event: %s")
+				errChannel <- fmt.Errorf("can't handle SA from watching event")
+				return
+			}
+			if serviceAccount.GetName() != smServiceAccountName {
 				continue
 			}
-			secret, err := clientset.CoreV1().Secrets(namespace).Get(context.Background(), secretRef.Name, metav1.GetOptions{})
-			if err != nil {
-				logger.Errorf("Can't get secret with name %s from namespace %s: %s", secretRef.Name, namespace, err)
-				errChannel <- fmt.Errorf("can't handle token from secret %s for SA %s", secretRef.Name, smServiceAccountName)
+			if event.Type == watch.Added || event.Type == watch.Modified {
+				secretRef := utils.FindFirstFromSlice(serviceAccount.Secrets, func(secretRef corev1.ObjectReference) bool {
+					return strings.Contains(secretRef.Name, "token")
+				})
+				if secretRef == nil {
+					logger.Warnf("Secret for appropriate SA is not ready yet")
+					continue
+				}
+				secret, err := clientset.CoreV1().Secrets(namespace).Get(context.Background(), secretRef.Name, metav1.GetOptions{})
+				if err != nil {
+					logger.Errorf("Can't get secret with name %s from namespace %s: %s", secretRef.Name, namespace, err)
+					errChannel <- fmt.Errorf("can't handle token from secret %s for SA %s", secretRef.Name, smServiceAccountName)
+					return
+				}
+				if btoken, found := secret.Data["token"]; !found {
+					logger.Errorf("Can't get token from secret with name %s from namespace %s: %s", secretRef.Name, namespace, err)
+					errChannel <- fmt.Errorf("can't handle token from secret %s for SA %s", secretRef.Name, smServiceAccountName)
+					return
+				} else {
+					token = string(btoken)
+				}
+				logger.Infof("Service-account %s was %s. Token was updated.", smServiceAccountName, event.Type)
+			} else if event.Type == watch.Deleted {
+				logger.Errorf("Service-account %s was deleted. Exit", smServiceAccountName)
+				errChannel <- fmt.Errorf("service-account %s was deleted", smServiceAccountName)
 				return
 			}
-			btoken, found := secret.Data["token"]
-			if !found {
-				logger.Errorf("Can't get token from secret with name %s from namespace %s: %s", secretRef.Name, namespace, err)
-				errChannel <- fmt.Errorf("can't handle token from secret %s for SA %s", secretRef.Name, smServiceAccountName)
-				return
-			}
-			TokenChannel <- string(btoken)
-			logger.Infof("Service-account %s was %s. Token was updated.", smServiceAccountName, event.Type)
-		} else if event.Type == watch.Deleted {
-			logger.Errorf("Service-account %s was deleted. Exit", smServiceAccountName)
-			errChannel <- fmt.Errorf("service-account %s was deleted", smServiceAccountName)
-			return
 		}
 	}
 }
--- site-manager-cr-controller/pkg/model/sm_testing.go
+++ site-manager-cr-controller/pkg/model/sm_testing.go
@@ -2,7 +2,6 @@ package model
 
 // SMConfig represents the debug configuration, that is used in SM_CONFIG_FILE
 type SMConfig struct {
-	Token        string          `yaml:"token"`
 	Testing      SMConfigTesting `yaml:"testing"`
 	TokenChannel chan string     `yaml:"-"`
 }
@@ -10,17 +9,14 @@ type SMConfig struct {
 // SMConfigTesting represents the testing field from SMConfig
 type SMConfigTesting struct {
 	Enabled bool         `yaml:"enabled"`
+	Token   string       `yaml:"token"`
 	SMDict  SMDictionary `yaml:"sm_dict"`
 }
 
 // GetToken handles token changes from channel and return the last one
 func (smc *SMConfig) GetToken() string {
-	for {
-		select {
-		case token := <-smc.TokenChannel:
-			smc.Token = token
-		default:
-			return smc.Token
-		}
+	if smc.Testing.Enabled {
+		return smc.Testing.Token
 	}
+	return <-smc.TokenChannel
 }
